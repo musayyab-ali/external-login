5e89796342da519888ab5e99d7421264c8a0144d
Musayyab Ali <meharmusayyab713@gmail.com>
2021-11-18 22:56:33 -0800

    Implemented Facebook Login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FacebookLogin from "react-facebook-login";
 
 function App() {
+  const responseFacebook = (response) => {
+    console.log(response);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FacebookLogin
+          appId="639360217231205"
+          autoLoad={false}
+          fields="name,email,picture"
+          callback={responseFacebook}
+        />
       </header>
     </div>
   );
